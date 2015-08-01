b7aa3e98ef398ea2ed923e37d1fd9293932ba2e5
sebug <sebastian.gfeller@gmail.com>
2015-08-01 19:18:20 +0200

    Getting actual directory content

--- Main.hs
+++ Main.hs
@@ -1,8 +1,9 @@
 module Main where
 
 import System.Environment ( getArgs )
-import System.Exit (exitWith, ExitCode( ExitFailure ) )
+import System.Exit ( exitWith, ExitCode( ExitFailure ) )
 import System.IO ( hPutStrLn, stderr )
+import System.Directory ( getDirectoryContents )
 
 main :: IO ()
 main = do
@@ -11,5 +12,8 @@ main = do
     [] -> do
           hPutStrLn stderr "Usage: TranslationLister sourceDirectory"
           exitWith (ExitFailure 1)
-    (dir : _) -> putStrLn dir
+    (dir : _) -> do
+      cts <- getDirectoryContents dir
+      putStrLn (show $ head cts)
+
 
