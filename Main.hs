08a0859f3080a6435538115177dae413cc2d6c65
sebug <sebastian.gfeller@gmail.com>
2015-08-01 19:54:53 +0200

    Moduling out a bit

--- Main.hs
+++ Main.hs
@@ -1,24 +1,10 @@
 module Main where
 
-import Control.Monad ( forM )
 import System.Environment ( getArgs )
-import System.Exit ( exitWith, ExitCode( ExitFailure ) )
 import System.IO ( hPutStrLn, stderr )
-import System.Directory ( getDirectoryContents, doesFileExist, doesDirectoryExist )
-import System.FilePath ( (</>) )
+import System.Exit ( exitWith, ExitCode( ExitFailure ) )
 
--- Directly lifted from http://book.realworldhaskell.org/read/io-case-study-a-library-for-searching-the-filesystem.html
-getRecursiveContents :: FilePath -> IO [FilePath]
-getRecursiveContents topdir = do
-  names <- getDirectoryContents topdir
-  let properNames = filter (`notElem` [".", ".."]) names
-  paths <- forM properNames $ \name -> do
-    let path = topdir </> name
-    isDirectory <- doesDirectoryExist path
-    if isDirectory
-      then getRecursiveContents path
-      else return [path]
-  return (concat paths)
+import TranslationLister
 
 main :: IO ()
 main = do
--- TranslationLister.hs
+++ TranslationLister.hs
@@ -1,3 +1,26 @@
 module TranslationLister where
 
+import Control.Monad ( forM )
+import System.Directory ( getDirectoryContents, doesFileExist, doesDirectoryExist )
+import System.FilePath ( (</>) )
+import Data.List ( isSuffixOf )
+
+-- Directly lifted from http://book.realworldhaskell.org/read/io-case-study-a-library-for-searching-the-filesystem.html
+getRecursiveContents :: FilePath -> IO [FilePath]
+getRecursiveContents topdir = do
+  names <- getDirectoryContents topdir
+  let properNames = filter (`notElem` [".", ".."]) names
+  paths <- forM properNames $ \name -> do
+    let path = topdir </> name
+    isDirectory <- doesDirectoryExist path
+    if isDirectory
+      then getRecursiveContents path
+      else return [path]
+  return (concat paths)
+
+getResxFiles :: FilePath -> IO [FilePath]
+getResxFiles topdir = do
+  files <- getRecursiveContents topdir
+  let resxs = filter (isSuffixOf ".resx") files
+  return resxs
 
