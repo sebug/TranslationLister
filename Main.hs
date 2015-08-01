8f8d7e21196b1d11476063b47b57e41fb5aa3278
sebug <sebastian.gfeller@gmail.com>
2015-08-01 18:04:22 +0200

    Good command line citizen

--- Main.hs
+++ Main.hs
@@ -1,11 +1,15 @@
 module Main where
 
 import System.Environment ( getArgs )
+import System.Exit (exitWith, ExitCode( ExitFailure ) )
+import System.IO ( hPutStrLn, stderr )
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [] -> putStrLn "Usage: TranslationLister sourceDirectory"
+    [] -> do
+          hPutStrLn stderr "Usage: TranslationLister sourceDirectory"
+          exitWith (ExitFailure 1)
     (dir : _) -> putStrLn dir
 
