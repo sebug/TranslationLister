40766eda0c6df0c8f31e5db6297a3798400bc4e6
sebug <sebastian.gfeller@gmail.com>
2015-08-01 19:43:20 +0200

    Recursively listing files

--- Main.hs
+++ Main.hs
@@ -1,9 +1,24 @@
 module Main where
 
+import Control.Monad ( forM )
 import System.Environment ( getArgs )
 import System.Exit ( exitWith, ExitCode( ExitFailure ) )
 import System.IO ( hPutStrLn, stderr )
-import System.Directory ( getDirectoryContents )
+import System.Directory ( getDirectoryContents, doesFileExist, doesDirectoryExist )
+import System.FilePath
+
+-- Directly lifted from http://book.realworldhaskell.org/read/io-case-study-a-library-for-searching-the-filesystem.html
+getRecursiveContents :: FilePath -> IO [FilePath]
+getRecursiveContents topdir = do
+  names <- getDirectoryContents topdir
+  let properNames = filter (`notElem` [".", ".."]) names
+  paths <- forM properNames $ \name -> do
+    let path = topdir </> name
+    isDirectory <- doesDirectoryExist path
+    if isDirectory
+      then getRecursiveContents path
+      else return [path]
+  return (concat paths)
 
 main :: IO ()
 main = do
@@ -13,7 +28,7 @@ main = do
           hPutStrLn stderr "Usage: TranslationLister sourceDirectory"
           exitWith (ExitFailure 1)
     (dir : _) -> do
-      cts <- getDirectoryContents dir
-      putStrLn (show $ head cts)
+      cts <- getRecursiveContents dir
+      putStrLn (show $ cts)
 
 
