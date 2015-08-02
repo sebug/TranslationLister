f7c4e803b5ae06a2a7905981a51bfaf213d20a94
sebug <sebastian.gfeller@gmail.com>
2015-08-02 11:30:23 +0200

    Parsing XML now

--- Main.hs
+++ Main.hs
@@ -4,6 +4,8 @@ import System.Environment ( getArgs )
 import System.IO ( hPutStrLn, stderr )
 import System.Exit ( exitWith, ExitCode( ExitFailure ) )
 
+import Text.XML.HXT.Parser.XmlParsec
+
 import TranslationLister ( getResxFiles, resxFileGroups, resxFileLanguage )
 
 main :: IO ()
@@ -17,6 +19,11 @@ main = do
       cts <- getResxFiles dir
       let resxGroups = resxFileGroups cts in
         let groupsWithTranslations = filter (\l -> length l > 1) resxGroups in
-        putStrLn (show groupsWithTranslations)
+        let (veryFirstPath,_) = head $ head groupsWithTranslations in
+        do
+          content <- readFile veryFirstPath
+          let contentWithoutBom = tail content in
+            let contentAsXml = xreadDoc contentWithoutBom in
+            putStrLn (show contentAsXml)
 
 
