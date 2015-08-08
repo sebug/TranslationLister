29e0104ae56aa0fdb2861a638e134cef4896f845
sebug <sebastian.gfeller@gmail.com>
2015-08-08 11:18:22 +0200

    Preparing for playing around with HXT

--- Main.hs
+++ Main.hs
@@ -4,9 +4,21 @@ import System.Environment ( getArgs )
 import System.IO ( hPutStrLn, stderr )
 import System.Exit ( exitWith, ExitCode( ExitFailure ) )
 
-import Text.XML.HXT.Parser.XmlParsec
+import Text.XML.HXT.Parser.XmlParsec ( xreadDoc )
+import Text.XML.HXT.DOM.TypeDefs ( XmlTrees, XmlTree, XNode(..) )
+import Text.XML.HXT.DOM.QualifiedName ( localPart )
+import Data.Tree.NTree.TypeDefs ( NTree(..) )
 
 import TranslationLister ( getResxFiles, resxFileGroups, resxFileLanguage )
+
+type TranslationEntry = (String,String)
+
+extract_entries :: XmlTree -> [TranslationEntry]
+extract_entries tree = []
+
+is_root :: XmlTree -> Bool
+is_root (NTree (XTag rootqname _) _) = localPart rootqname == "root"
+is_root _ = False
 
 main :: IO ()
 main = do
@@ -24,6 +36,11 @@ main = do
           content <- readFile veryFirstPath
           let contentWithoutBom = tail content in
             let contentAsXml = xreadDoc contentWithoutBom in
-            putStrLn (show contentAsXml)
+            let roots = filter is_root contentAsXml in
+            case roots of
+              [] -> putStrLn "No entries found"
+              [r] -> putStrLn (show r)
+              _ -> putStrLn "Too many roots found"
+
 
 
